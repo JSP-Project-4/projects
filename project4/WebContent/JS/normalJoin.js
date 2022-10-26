44b465d7ff1db3f29e0287c7f92ac873dd3f87d5
user1 <user1@DESKTOP-50SGODH>
2022-10-26 17:57:49 +0900

    10-26 유저 세션 코드 생성

--- project4/WebContent/JS/normalJoin.js
+++ project4/WebContent/JS/normalJoin.js
@@ -9,33 +9,18 @@
 	});
 	
 	$("#signUpbtn").on("click", function() {
-		if($(".lname").val() == '' || $(".fname").val() == '') {
+		if($("#lname").val() == '' || $("#fname").val() =='') {
 			alert("이름을 입력해주세요.");
 			return false;
 		}
-		else if($(".bir").val() == '') {
+		else if($("#bir").val() == '') {
 			alert("생년월일을 입력해주세요.");
 			return false;
-		} else if($(".phone").val() == '') { 
+		} else if($("#ph").val() == '') { 
 			alert("번호를 입력해주세요.");
 			return false;			
 		} else {
-			$.ajax({
-				url: "Projects4/signUp.do",
-				data: $("#inform").serialize(),
-				datatype: "text",
-				success: function(data) {
-					if(data > 0) {
-						location.href=""; // 회원가입이 완료되었을때 이동경로
-					}
-					else {
-						alert("오 류");
-					}
-				},
-				error: function() {
-					alert("통신 오류")
-				}
-			});	// $.ajax({
+			$("#inform").submit();
 		}
 	});	 // $("#signUpbtn")
 		
